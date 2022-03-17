db927e68eee4c64fc5137d11cfe26b0b35c64ba3
gogy9000 <goropashnyj9000@gmail.com>
2022-03-17 13:27:05 +0500

    Accordion and Rating

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,47 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <Title  />
 
-    </div>
-  );
+    return (
+        <div className="App">
+            <Title/>
+            <Accordion/>
+        </div>
+    );
 }
 
-export const Title=(props:any)=>{
-    return(
+export const Title = () => {
+    return (
 
-    props.boo?<h1>Title</h1>:<h1>notTitle</h1>
-    )}
+        <h1>notTitle</h1>
+    )
+}
+export const Rating = () => {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+
+        </>
+    )
+}
+export const Accordion = () => {
+    return (
+        <div>
+            <Rating/>
+        </div>
+    )
+}
+export const Star = () => {
+    return (
+        <div>
+           star
+        </div>
+    )
+}
 
 export default App;
