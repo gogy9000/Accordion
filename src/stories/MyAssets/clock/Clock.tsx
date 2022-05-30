da002cb7b17c4e92dfbb3e909df77f994bafa7d5
gogy9000 <goropashnyj9000@gmail.com>
2022-05-30 16:34:01 +0500

    refactoring Clock.tsx

--- src/stories/MyAssets/clock/Clock.tsx
+++ src/stories/MyAssets/clock/Clock.tsx
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from "react";
-import {Grid, Paper, Tooltip, Typography} from "@mui/material";
+import {Grid, Tooltip, Typography} from "@mui/material";
 
 type ClockContainerPropsType = {
     ClockLabel: string
     width?: number
-
 }
 
 export const ClockContainer: React.FC<ClockContainerPropsType> = (props) => {
-    const [date, setDate] = useState<Date>(new Date())
+
+    const initDate = () => new Date()
+
+    const [date, setDate] = useState<Date>(initDate)
 
     useEffect(() => {
         setTimeout(() => {
@@ -16,15 +18,14 @@ export const ClockContainer: React.FC<ClockContainerPropsType> = (props) => {
         }, 1000)
     }, [date])
 
-    return (
-        <>
-            <Clock label={props.ClockLabel} date={date} width={props.width}/>
-        </>
-    )
+    return <Clock label={props.ClockLabel} date={date} width={props.width}/>
+
+
 }
 type ClockPropsType = {
     label: string
     date: Date
+
     width?: number
 }
 export const Clock: React.FC<ClockPropsType> = ({label, date, width}) => {
@@ -33,30 +34,26 @@ export const Clock: React.FC<ClockPropsType> = ({label, date, width}) => {
         const minutes = date.getMinutes()
         const seconds = date.getSeconds()
         const hours = date.getHours()
-
-        const time = `${hours < 10 ? 0 : ''}${hours}:${minutes < 10 ? 0 : ''}${minutes}:${seconds < 10 ? 0 : ''}${seconds}`
-
-        return time
+        return `${hours < 10 ? 0 : ''}${hours}:${minutes < 10 ? 0 : ''}${minutes}:${seconds < 10 ? 0 : ''}${seconds}`
     }
 
     return (
 
         <Typography component={'div'}>
             <Tooltip title={date.toLocaleDateString()}
-                     enterDelay={300} leaveDelay={500} >
+                     enterDelay={300} leaveDelay={500}>
+
                 <Grid container
                       direction={'column'}
                       justifyContent={'center'}
                       alignItems={'center'}
                       bgcolor={'darkgray'}
                       borderRadius={3}
-                      width={width?width:100} >
+                      width={width ? width : 100}>
 
-                    <Grid >{label}</Grid>
+                    <Grid>{label}</Grid>
 
-                    <Grid >{getTime()}</Grid>
-
-
+                    <Grid>{getTime()}</Grid>
                 </Grid>
             </Tooltip>
         </Typography>
